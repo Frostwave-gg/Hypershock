3f25d9c3aa3031e1c94c0414874e6ca080dc63ed
Fr0zen3k <jankresic@gmail.com>
2022-08-02 20:16:07 +0200

    Initial branch setup

---
+++ Hypershock/src/Hypershock/Core/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jankr on 02-Aug-22.
+//
+
+#include "Application.hpp"
---
+++ Hypershock/src/Hypershock/Core/Application.hpp
@@ -0,0 +1,20 @@
+//
+// Created by jankr on 02-Aug-22.
+//
+
+#ifndef HYPERSHOCK_GAME_APPLICATION_HPP
+#define HYPERSHOCK_GAME_APPLICATION_HPP
+
+#include "Types.hpp"
+#include "Core.hpp"
+#include "Event/Event.hpp"
+
+namespace Hypershock {
+
+    class Application {
+
+    };
+
+}
+
+#endif //HYPERSHOCK_GAME_APPLICATION_HPP
