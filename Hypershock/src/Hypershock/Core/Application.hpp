738e80848a2ebd26af64b55efdf852730a5ae70d
Fr0zen3k <jankresic@gmail.com>
2022-08-06 21:47:58 +0200

    Initial app setup

--- Hypershock/src/Hypershock/Core/Application.hpp
+++ Hypershock/src/Hypershock/Core/Application.hpp
@@ -8,11 +8,27 @@
 #include "Types.hpp"
 #include "Core.hpp"
 #include "Event/Event.hpp"
+#include "Window.hpp"
+#include "Monitor.hpp"
+
+#include <string>
+#include <vector>
+#include <map>
 
 namespace Hypershock {
 
     class Application {
+    public:
+        Application();
+        virtual ~Application();
 
+        std::string NewWindow();
+        Window& GetWindow(const std::string& id);
+
+    private:
+        std::map<std::string, Window*> m_Windows;
+        std::vector<Monitor> m_Monitors;
+        Window* m_MainWindow;
     };
 
 }
